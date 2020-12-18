82dfb338b107693a4aab065934d89bae21481ed8
zz <zz@zzs-Mac-mini.local>
2020-12-18 15:38:55 +0100

    update the code

--- mac.sh
+++ mac.sh
@@ -1,53 +1,9 @@
 #!/bin/bash
-export MAC_ROOT=${HOME}/shell/
-#export PAGER=most
-
-## brew
-## Installing byobu dependency: coreutils
-PATH="${WORK_BASE}/tool-kit/publics/homebrew/opt/coreutils/libexec/gnubin:$PATH"
-MANPATH="${WORK_BASE}/tool-kit/publics/homebrew/opt/coreutils/libexec/gnuman:$MANPATH"
-## Installing byobu dependency: gnu-sed
-PATH="${WORK_BASE}/tool-kit/publics/homebrew/opt/gnu-sed/libexec/gnubin:$PATH"
-MANPATH="${WORK_BASE}/tool-kit/publics/homebrew/opt/gnu-sed/libexec/gnuman:$MANPATH"
-## libxml2
-export PATH="${WORK_BASE}/tool-kit/publics/homebrew/opt/libxml2/bin:$PATH"
-## apr
-export PATH="${WORK_BASE}/tool-kit/publics/homebrew/opt/apr/bin:$PATH"
-## ccache
-export PATH="${WORK_BASE}/tool-kit/publics/homebrew/opt/ccache/libexec:$PATH"
-## curl
-export PATH="${WORK_BASE}/tool-kit/publics/homebrew/opt/curl/bin:$PATH"
-## e2fsprogs
-export PATH="${WORK_BASE}/tool-kit/publics/homebrew/opt/e2fsprogs/bin:$PATH"
-export PATH="${WORK_BASE}/tool-kit/publics/homebrew/opt/e2fsprogs/sbin:$PATH"
-## findutils
-export PATH="${WORK_BASE}/tool-kit/publics/homebrew/opt/findutils/libexec/gnubin:$PATH"
-export MANPATH="${WORK_BASE}/tool-kit/publics/homebrew/opt/findutils/libexec/gnuman:$MANPATH"
-## flex
-export PATH="${WORK_BASE}/tool-kit/publics/homebrew/opt/flex/bin:$PATH"
-## docbook
-export XML_CATALOG_FILES="${WORK_BASE}/tool-kit/publics/homebrew/etc/xml/catalog"
-## end
-
-## Java Einstellungen
-unset JAVA_HOME
-export JAVA_HOME=/Library/Java/Home
-# export JRE_HOME=${JAVA_HOME}/jre
-# export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
-export PATH=${JAVA_HOME}/bin:${PATH}
-## end
-
-# PS1 config
-export PS1=${PS_BCYAN}'[\u@\h: '${PS_BGREEN}'\w '${PS_BYELLOW}'➜ '${PS_BCYAN}'$(__git_ps1 " (${PS_BRED}%s${PS_BCYAN})")'${PS_BCYAN}']\n$ '${PS_COLOR_NC}
-# '\[\e[36;1m\]\u@\h: \[\e[32;1m\]\w\[\e[36;1m\]\$ \[\e[0m\]'
-# '[ \u@\h \W$(__git_ps1 " (%s)")]\$ '
-# end
-
 ## for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
-unset HISTSIZE
-unset HISTFILESIZE
-export HISTSIZE=1000000000
-export HISTFILESIZE=2000000000
-export HISTTIMEFORMAT="[%F %T] "
-export HISTCONTROL=ignoreboth
+#unset HISTSIZE
+#unset HISTFILESIZE
+#export HISTSIZE=1000000000
+#export HISTFILESIZE=2000000000
+#export HISTTIMEFORMAT="[%F %T] "
+#export HISTCONTROL=ignoreboth
 ## end
--- path.sh
+++ path.sh
@@ -1,31 +1,11 @@
 #!/bin/bash
-# development einstellungen
-## value the varables
-## eclipse envalue
-#export PATH=${ECLIPSE_HOME}:/sbin:/usr/sbin:$PATH
-#export PATH=${MSHELL}/run:${MSHELL}/bin:$PATH
-## end
 
-## works/bin
-export PATH=${LOCAL_WORKS_DIR}/bin:${PATH}
-## end
-# end
-
-# homebrew software envalue
+# brew software envalue
 case ${ARCH} in
 	'Darwin')
-		export PATH=${LOCAL_WORKS_DIR}/tool-kit/publics/homebrew/bin:${PATH}
-		export PATH=${LOCAL_WORKS_DIR}/tool-kit/publics/homebrew/sbin:${PATH}
-        export PATH=${LOCAL_WORKS_DIR}/tool-kit/publics/homebrew/opt/apr/bin:${PATH}
-        export PATH=${LOCAL_WORKS_DIR}/tool-kit/publics/homebrew/opt/openssl/bin:$PATH
-		## for macports command path
-		export PATH=/opt/local/bin:$PATH
-        ## for works/bin self define bin
-        export PATH=${LOCAL_WORKS_DIR}/bin:${PATH}
-        ## laraval
-        export PATH=$HOME/.composer/vendor/bin:${PATH}
-		;;
-	'Linux')
+		export PATH=${HOME}/brew/bin:${PATH}
+		export PATH=${HOME}/brew/sbin:${PATH}
+		export PATH=/usr/local/bin:$PATH
 		;;
 esac
 # end
